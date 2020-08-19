0113216d1836904881029316510e27682ff9a722
DOGUK <dgkim1994@gmail.com>
2020-08-19 19:21:16 +0900

    AddCounterWithHooksAndTS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from "react";
-import { createGlobalStyle } from "styled-components";
+import React, { useState } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState(0);
+  const add = () => setCount(count + 1);
+  const min = () => setCount(count - 1);
+  return (
+    <>
+      <p>The Number is {count}</p>
+      <button onClick={add}>+</button>
+      <button onClick={min}>-</button>
+    </>
+  );
 }
 
 export default App;
